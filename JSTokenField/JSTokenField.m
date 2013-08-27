f4db01d01116e80e726b2a7c26cbd3ae4fb17c36
Greg Musick <greg@substantial.com>
2013-08-27 09:58:36 -0700

    Increase line spacing

--- JSTokenField/JSTokenField.m
+++ JSTokenField/JSTokenField.m
@@ -36,7 +36,7 @@ NSString *const JSTokenFieldNewFrameKey = @"JSTokenFieldNewFrameKey";
 NSString *const JSTokenFieldOldFrameKey = @"JSTokenFieldOldFrameKey";
 NSString *const JSDeletedTokenKey = @"JSDeletedTokenKey";
 
-#define HEIGHT_PADDING 0
+#define HEIGHT_PADDING 10
 #define WIDTH_PADDING 3
 
 #define DEFAULT_HEIGHT 31
